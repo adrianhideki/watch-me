020c20591966bcf4fdc868288acee8b7360c8610
Adrian Hideki <adrian.hideki.br@gmail.com>
2021-05-07 20:44:44 -0300

    Criando componentes SideBar e Content

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,35 @@
-export function Content() {
+import React from 'react';
+import { MovieCard } from "./MovieCard";
+import { MovieProps } from '../types'
+
+type ContentType = {
+  title: string;
+  movies: MovieProps[]
+}
+
+export function Content({ title, movies } : ContentType) {
   // Complete aqui
+  return (
+    <div className="container">
+      <header>
+        <span className="category">
+          Categoria:<span> {title}</span>
+        </span>
+      </header>
+
+      <main>
+        <div className="movies-list">
+          {movies.map((movie) => (
+            <MovieCard
+              key={movie.imdbID}
+              title={movie.Title}
+              poster={movie.Poster}
+              runtime={movie.Runtime}
+              rating={movie.Ratings[0].Value}
+            />
+          ))}
+        </div>
+      </main>
+    </div>
+  );
 }
--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,36 @@
-export function SideBar() {
+import React from "react";
+import { Button } from "./Button";
+import { GenreResponseProps } from "../types";
+
+type SideBarProps = {
+  genres: GenreResponseProps[];
+  selectedGenreId: number;
+  handleClickButton: (id: number) => void;
+};
+
+export function SideBar({
+  genres,
+  handleClickButton,
+  selectedGenreId,
+}: SideBarProps) {
   // Complete aqui
+  return (
+    <nav className="sidebar">
+      <span>
+        Watch<p>Me</p>
+      </span>
+
+      <div className="buttons-container">
+        {genres.map((genre) => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => handleClickButton(genre.id)}
+            selected={selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+    </nav>
+  );
 }
